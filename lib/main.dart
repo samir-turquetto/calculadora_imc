d2f34fa72ecc930192b5c5836a1010bc88d68277
Samir Turquetto Melo <samir.melo@totvs.com.br>
2021-12-16 08:40:39 -0300

    :sparkle: ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    title: "Calculadora de IMC",
     home: Home(),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
@@ -14,6 +16,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   TextEditingController weightController = TextEditingController();
   TextEditingController heightController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
 
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
@@ -22,6 +25,7 @@ class _HomeState extends State<Home> {
   void _resetFields() {
     weightController.text = "";
     heightController.text = "";
+    nameController.text = "";
     setState(() {
       _infoText = "Informe seus dados!";
     });
@@ -32,18 +36,25 @@ class _HomeState extends State<Home> {
       double weight = double.parse(weightController.text);
       double height = double.parse(heightController.text) / 100;
       double imc = weight / (height * height);
+
       if (imc < 18.6) {
-        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
+        _infoText =
+            "${nameController.text} está abaixo do \npeso (${imc.toStringAsPrecision(4)})";
       } else if (imc >= 18.6 && imc < 24.9) {
-        _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)})";
+        _infoText =
+            "${nameController.text} está com o \npeso ideal (${imc.toStringAsPrecision(4)})";
       } else if (imc >= 24.9 && imc < 29.9) {
-        _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(4)})";
+        _infoText =
+            "${nameController.text} está \nlevemente acima do peso (${imc.toStringAsPrecision(4)})";
       } else if (imc >= 29.9 && imc < 34.9) {
-        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+        _infoText =
+            "${nameController.text} está com \nobesidade grau I (${imc.toStringAsPrecision(4)})";
       } else if (imc >= 34.9 && imc < 39.9) {
-        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+        _infoText =
+            "${nameController.text} está com \nobesidade grau II (${imc.toStringAsPrecision(4)})";
       } else if (imc >= 40) {
-        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+        _infoText =
+            "${nameController.text} está com \nobesidade grau III (${imc.toStringAsPrecision(4)})";
       }
     });
   }
@@ -71,6 +82,20 @@ class _HomeState extends State<Home> {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+              TextFormField(
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                    labelText: "Nome",
+                    labelStyle: TextStyle(color: Colors.green)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+                controller: nameController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Insira seu Nome!";
+                  }
+                },
+              ),
               TextFormField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
@@ -103,7 +128,7 @@ class _HomeState extends State<Home> {
                 padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
                 child: Container(
                   height: 50.0,
-                  child: RaisedButton(
+                  child: ElevatedButton(
                     onPressed: () {
                       if (_formKey.currentState!.validate()) {
                         _calculate();
@@ -113,7 +138,7 @@ class _HomeState extends State<Home> {
                       "Calcular",
                       style: TextStyle(color: Colors.white, fontSize: 25.0),
                     ),
-                    color: Colors.green,
+                    style: ElevatedButton.styleFrom(primary: Colors.green),
                   ),
                 ),
               ),
